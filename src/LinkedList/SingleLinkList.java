e8ba808b49bef3d8cc2f60341e103d603f3bba53
shakthishamruth <shakthishamruth@gmail.com>
2024-12-30 01:49:25 +0530

    update linkedlist

---
+++ src/LinkedList/SingleLinkList.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+class Node {
+    private int data;
+    private Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class SingleLinkList {
+
+    public static void main(String[] args) {
+        boolean running = true;
+
+        while (running) {
+            System.out.println("Choose: " +
+                    "1. View" +
+                    "2. Add");
+            
+        }
+    }
+}
