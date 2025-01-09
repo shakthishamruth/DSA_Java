8f9cce4b06af9ad4d121a8aee72aacf49c6cd102
shakthishamruth <shakthishamruth@gmail.com>
2025-01-09 18:24:46 +0530

    update stack minStack

--- src/Stack/MinimumStack.java
+++ src/Stack/MinimumStack.java
@@ -1,41 +1,81 @@
 package Stack;
 
-import java.util.Objects;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class MinimumStack {
-    Stack<Integer> stk = new Stack<>();
-    Stack<Integer> minStk = new Stack<>();
+    static Stack<Integer> stk = new Stack<>();
+    static Stack<Integer> minStk = new Stack<>();
 
-    void push(int n) {
+    static void push(int n) {
         stk.push(n);
+        if (minStk.isEmpty() || minStk.peek() >= n) {
+            minStk.push(n);
+        }
+        System.out.println("Number Inserted: " + n);
+    }
+
+    static void peek() {
+        if (stk.isEmpty()) {
+            System.out.println("Stack is empty!");
+            return;
+        }
+        System.out.println("Top most element: " + stk.peek());
+    }
+
+    static void getMin() {
         if (minStk.isEmpty()) {
-            minStk.push(n);
-        } else if (minStk.peek() > n) {
-            minStk.push(n);
+            System.out.println("Stack is empty!");
+        } else {
+            System.out.println("Minimum element: " + minStk.peek());
         }
     }
 
-    void peek() {
-        System.out.println(stk.peek());
-    }
+    static void pop() {
+        if (stk.isEmpty()) {
+            System.out.println("Stack is empty!");
+            return;
+        }
 
-    void minInt() {
-        System.out.println(minStk.peek());
-    }
+        int t = stk.pop();
+        System.out.println("Removed element: " + t);
 
-    void pop() {
-        if (!stk.isEmpty()) {
-            if (Objects.equals(minStk.peek(), stk.peek())) {
-                minStk.pop();
-                stk.pop();
-            } else {
-                stk.pop();
-            }
+        if (t == minStk.peek()) {
+            minStk.pop();
         }
     }
 
     public static void main(String[] args) {
-        System.out.println();
+        boolean running = true;
+        Scanner sc = new Scanner(System.in);
+        while (running) {
+            System.out.println("----------------------------------------------------");
+            System.out.println("[0.exit] [1. push] [2.peek] [3.pop] [4.getMin]");
+            int ch = sc.nextInt();
+            switch (ch) {
+                case 0:
+                    System.out.println("Exiting...");
+                    running = false;
+                    break;
+                case 1:
+                    System.out.print("Enter the int to enter: ");
+                    int in = sc.nextInt();
+                    push(in);
+                    break;
+                case 2:
+                    peek();
+                    break;
+                case 3:
+                    pop();
+                    break;
+                case 4:
+                    getMin();
+                    break;
+                default:
+                    System.out.println("Wrong choice try again!");
+                    break;
+            }
+        }
+        sc.close();
     }
 }
